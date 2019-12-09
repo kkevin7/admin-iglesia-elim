d0572afb61658cb5570515fb09f30bfb60a9e3a3
Kevin Martinez <kkevinmartinez7@gmail.com>
2019-12-09 09:56:25 -0600

    audix fix, update some dependeces and  firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,29 +1,32 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 // Initialize Firebase
 const config = {
-  apiKey: 'AIzaSyCYaTKjfam_qMXDnGfcdnBxScEq89VQtLk',
-  authDomain: 'curious-sandbox-196209.firebaseapp.com',
-  databaseURL: 'https://curious-sandbox-196209.firebaseio.com',
-  projectId: 'curious-sandbox-196209',
-  storageBucket: '',
-  messagingSenderId: '1034032747860'
+  // Your web app's Firebase configuration
+  apiKey: "AIzaSyCbvPGqnV8rE5OJuHeAi4-oFhL8RJQ2oEc",
+  authDomain: "iglesia-elim-db.firebaseapp.com",
+  databaseURL: "https://iglesia-elim-db.firebaseio.com",
+  projectId: "iglesia-elim-db",
+  storageBucket: "iglesia-elim-db.appspot.com",
+  messagingSenderId: "299275738665",
+  appId: "1:299275738665:web:8eafc1c0835c08b9d90638"
 };
 
 firebase.initializeApp(config);
+
+// const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+// const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+// const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+// const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
 const auth = firebase.auth();
-
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
-const githubAuthProvider = new firebase.auth.GithubAuthProvider();
-const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
-
 const database = firebase.database();
+
 export {
+  firebase,
   auth,
-  database,
-  googleAuthProvider,
-  githubAuthProvider,
-  facebookAuthProvider,
-  twitterAuthProvider
+  database
+  // googleAuthProvider,
+  // githubAuthProvider,
+  // facebookAuthProvider,
+  // twitterAuthProvider
 };
